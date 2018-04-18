bfeeed9601599594578f59d6774beb39bd55bc4b
Dave Compton <davecompton7@gmail.com>
2018-04-18 20:09:42 +0000

    rewrote decorate function for clarity

--- Test.hs
+++ Test.hs
@@ -7,9 +7,6 @@ import Data.Set
 import Data.List.Split
 import Data.List
 
-decorate :: Set Spot -> [((Int,Int),Char)]
-decorate spots = (\loc -> (loc,getName spots) ) <$> getLocations spots
-
 main = do
   let pieces =
         [ ['I', 'P', 'P', 'Y', 'Y', 'Y', 'Y', 'V', 'V', 'V']
@@ -21,17 +18,26 @@ main = do
         ]
       board = [(row, col) | row <- [0 .. 11], col <- [0 .. 4]]
 
+      zeroProgress :: Progress Spot
       zeroProgress = initialProgress board pieces 
 
+      firstSolution :: [Piece]
       firstSolution =   used -- the pieces used for the first solution
                       $ head -- the first solution
                       $ solutions -- get the list of solutions
                       $ zeroProgress -- no progress yet
 
+  mapM_ print firstSolution
+
+  let decorate piece = [ (loc,name) | loc <- getLocations piece, let name = getName piece ]
+
+      prettySolution :: [[Char]]
       prettySolution =  chunksOf 5 -- 5 columns at a time for printing.
                       $ fmap snd   -- Drop the locations - keep the names.
                       $ sort       -- Sort by location.
                       $ concat     -- gather all the named locations 
                       $ decorate <$> firstSolution -- Add names to locations
+  return ()
 
-  mapM_ print prettySolution
+  -- mapM_ print prettySolution
+
