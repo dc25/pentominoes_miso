e82e828c2accae99a13915d16346b76fd2cbf6bd
Dave Compton <davecompton7@gmail.com>
2018-04-21 00:23:05 +0000

    copied some code from Test.hs into Main.hs

--- Main.hs
+++ Main.hs
@@ -46,25 +46,30 @@ instance Eq Model where
 main :: IO ()
 main = do
   t <- now
-  let
-    pieces =
-      [ ['I', 'P', 'P', 'Y', 'Y', 'Y', 'Y', 'V', 'V', 'V']
-      , ['I', 'P', 'P', 'X', 'Y', 'L', 'L', 'L', 'L', 'V']
-      , ['I', 'P', 'X', 'X', 'X', 'F', 'Z', 'Z', 'L', 'V']
-      , ['I', 'T', 'W', 'X', 'F', 'F', 'F', 'Z', 'U', 'U']
-      , ['I', 'T', 'W', 'W', 'N', 'N', 'F', 'Z', 'Z', 'U']
-      , ['T', 'T', 'T', 'W', 'W', 'N', 'N', 'N', 'U', 'U']
-      ]
-    board = [(row, col) | row <- [0 .. 11], col <- [0 .. 4]]
+  let pieces =
+        [ ['I', 'P', 'P', 'Y', 'Y', 'Y', 'Y', 'V', 'V', 'V']
+        , ['I', 'P', 'P', 'X', 'Y', 'L', 'L', 'L', 'L', 'V']
+        , ['I', 'P', 'X', 'X', 'X', 'F', 'Z', 'Z', 'L', 'V']
+        , ['I', 'T', 'W', 'X', 'F', 'F', 'F', 'Z', 'U', 'U']
+        , ['I', 'T', 'W', 'W', 'N', 'N', 'F', 'Z', 'Z', 'U']
+        , ['T', 'T', 'T', 'W', 'W', 'N', 'N', 'N', 'U', 'U']
+        ]
+      board = [(row, col) | row <- [0 .. 11], col <- [0 .. 4]]
 
-    initialModel =
+      zeroProgress :: Progress Element
+      zeroProgress = initialProgress board pieces 
+
+      allSteps = (Solve.steps) zeroProgress
+
+      initialModel =
         Model
           { time = 0
-          , steps = Solve.steps $ initialProgress board pieces
+          , steps = allSteps
           , solutions = []
           , rate = Step
           , stepRequested = False
           }
+
   startApp
     App
       { model = initialModel
