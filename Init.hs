5a47c3edb70fb5bdb3d526553a704eb06475cd16
Dave Compton <davecompton7@gmail.com>
2018-04-21 00:10:45 +0000

    moved some code from Piece.hs to Init.hs

--- Init.hs
+++ Init.hs
@@ -1,4 +1,4 @@
-module Init ( initialProgress ) where
+module Init ( initialProgress, bounds ) where
 
 import Control.Monad
 import Data.List 
@@ -8,6 +8,33 @@ import Piece
 import Solve
 
 type Board = Set Element
+
+rotate :: [(Int,Int)] -> [(Int,Int)]
+rotate = fmap (\(row,col) -> (col, -row)) 
+
+reflect :: [(Int,Int)] -> [(Int,Int)]
+reflect = fmap (\(row,col) -> (col, row)) 
+
+translate :: (Int, Int) -> [(Int,Int)] -> [(Int,Int)]
+translate (rowTrans, colTrans) = fmap (\(row,col) -> (row+rowTrans, col+colTrans)) 
+
+-- rotations and reflected rotations of a piece.
+variants :: [(Int,Int)] -> [[(Int,Int)]]
+variants p0 =
+  let p1 = rotate p0
+      p2 = rotate p1
+      p3 = rotate p2
+      r0 = reflect p0
+      r1 = reflect p1
+      r2 = reflect p2
+      r3 = reflect p3
+  in [p0,p1,p2,p3,r0,r1,r2,r3]
+
+bounds :: [(Int,Int)] -> ((Int, Int), (Int, Int))
+bounds coords =
+  let rows = fmap fst coords
+      cols = fmap snd coords
+  in ((minimum rows, minimum cols), (maximum rows, maximum cols))
 
 -- all the placements of a piece on a board ; may be duplicates
 positions0 :: [(Int,Int)] -> [(Int,Int)]-> [[(Int,Int)]]
--- Piece.hs
+++ Piece.hs
@@ -1,4 +1,4 @@
-module Piece (Element(..), Piece, getName, getLocations, bounds, translate, variants) where
+module Piece (Element(..), Piece, getName, getLocations) where
 
 import Data.Set as DS
 
@@ -27,29 +27,3 @@ getLocation (Location lo) = lo
 getLocations :: Piece -> [(Int, Int)]
 getLocations = fmap getLocation . toList . DS.filter isLocation
 
-bounds :: [(Int,Int)] -> ((Int, Int), (Int, Int))
-bounds coords =
-  let rows = fmap fst coords
-      cols = fmap snd coords
-  in ((minimum rows, minimum cols), (maximum rows, maximum cols))
-
-rotate :: [(Int,Int)] -> [(Int,Int)]
-rotate = fmap (\(row,col) -> (col, -row)) 
-
-reflect :: [(Int,Int)] -> [(Int,Int)]
-reflect = fmap (\(row,col) -> (col, row)) 
-
-translate :: (Int, Int) -> [(Int,Int)] -> [(Int,Int)]
-translate (rowTrans, colTrans) = fmap (\(row,col) -> (row+rowTrans, col+colTrans)) 
-
--- rotations and reflected rotations of a piece.
-variants :: [(Int,Int)] -> [[(Int,Int)]]
-variants p0 =
-  let p1 = rotate p0
-      p2 = rotate p1
-      p3 = rotate p2
-      r0 = reflect p0
-      r1 = reflect p1
-      r2 = reflect p2
-      r3 = reflect p3
-  in [p0,p1,p2,p3,r0,r1,r2,r3]
